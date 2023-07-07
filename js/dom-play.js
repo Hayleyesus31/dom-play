82a02942738612a61579528d31f5a7733ddae0fe
Hayleyesus Gujo <12hayleloke@gmail.com>
2023-07-06 23:27:50 -0700

    dom play

---
+++ js/dom-play.js
@@ -0,0 +1,48 @@
+//alert("hi from DOM play!");
+
+
+/*
+
+create an app that on click actors lines are hilighted 
+ 
+With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+Un-highlight (normal or otherwise) all lines from all other actors in the play
+
+The function(s) created to do the work must accommodate any number of actors, and any number of additional lines added to the page
+
+ 
+HTML "hooks" may be added to the page,  for proper opreation
+
+
+*/
+
+//document.querySelector(".hamlet").getElementsByClassName.backgroundColor
+
+function highlight(el){
+
+    if( el.style.backgroundColor == 'white'){//orange
+        el.style.backgroundColor='orange'
+
+    }else{//white
+        el.style.backgroundColor='white'
+
+
+    }
+}
+    function highlightActor(actor){
+        alert("the current actor is:" + actor);
+    }
+    
+    function myAlert(){
+        alert("I'm clicked!");
+    }
+
+
+const spans = document.querySelectorAll("#play span");
+console.log(spans);
+for(const mySpan of spans){
+    //mySpan.addEventListener("click" ,myAlert);
+    mySpan.addEventListener("click" , function(ev){
+        highlightActor(mySpan.CDATA_SECTION_NODE.actor);
+    });
+}
